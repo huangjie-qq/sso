5974fb91184f8a321980063f99c9e47a12385fc7
huangjie-qq <1181217392@qq.com>
2021-10-30 23:14:26 +0800

    1.初始化项目

---
+++ attestation/src/main/java/com/xqm/AttestationApplication.java
@@ -0,0 +1,13 @@
+package com.xqm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttestationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttestationApplication.class, args);
+    }
+
+}
